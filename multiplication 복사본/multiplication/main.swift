3009849bea275fc09598269dae64d5bc801c0975
박준희 <junibibi04@gmail.com>
2019-04-02 19:17:32 +0900

    헬로 월드

---
+++ multiplication 복사본/multiplication/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  multiplication
+//
+//  Created by JH on 02/04/2019.
+//  Copyright © 2019 JH. All rights reserved.
+//
+
+import Foundation
+
+var str = "hello, ios World"
+
+print(str)
