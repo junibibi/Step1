681bd082f0040ddd7afbd311a3101ebe1d37b681
박준희 <junibibi04@gmail.com>
2019-04-02 19:35:36 +0900

    1-2 코드

--- multiplication 복사본/multiplication/main.swift
+++ multiplication 복사본/multiplication/main.swift
@@ -8,6 +8,24 @@
 
 import Foundation
 
+//1-1
 var str = "hello, ios World"
-
 print(str)
+
+
+//1-2
+print("2단")
+var secondNums = 0
+var second = 2
+for num in 1..<10 {
+    secondNums = second * num
+    print(secondNums)
+}
+print("3단")
+var thirdNums = 0
+var third = 3
+for num in 1..<10 {
+    thirdNums = third * num
+    print(thirdNums)
+}
+
